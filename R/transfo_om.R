60f51767528690747b25b53843135250b2702953
GARNIER Mathieu <mathieu.garnier@arcep.fr>
2022-05-06 10:38:18 +0200

    exemple et détails sur fonction

--- R/transfo_om.R
+++ R/transfo_om.R
@@ -2,19 +2,42 @@
 #'
 #' @title Transformer un territoire de France d'Outre-Mer pour le placer près de la France métropolitaine.
 #'
-#' @description Modifie le champ géometrie du sf dataframe en entrée.
+#' @description Modifie le champ géométrie du sf dataframe en entrée.
 #'
-#' @param shape_origine Table géographique (format sf dataframe) en entrée contenant les entités du territoire de France d'Outre-Mer.
+#' @param shape_origine Table géographique (classe sf dataframe) en entrée contenant les entités du territoire de France d'Outre-Mer. Les géométries de ces entités peuvent être de type point, ligne ou polygone.
 #' @param var_departement Nom du champ de la table géographique en entrée contenant le code du département de France d'Outre-Mer.
-#' @param type_transfo Version de la transformation à appliquer.
+#' @param type_transfo Version de la transformation à appliquer (une seule disponible pour l'instant : 'v1').
 #'
-#' @return Renvoie une table géographique (format sf dataframe) contenant les entités du territoire de France d'Outre-Mer replacés.
+#' @return Renvoie une table géographique (classe sf dataframe) contenant les entités du territoire de France d'Outre-Mer replacés.
 #'
 #' @importFrom dplyr filter rename
 #' @importFrom rlang sym
 #' @importFrom sf st_transform st_as_sf st_set_crs
 #' @importFrom maptools elide
 #'
+#' @examples
+#' \dontrun{
+#' # Transformer la géométrie des communes d'un territoire d'Outre-Mer pour les afficher proche de la France métropolitaine
+#' indics_migres_DEP <-
+#' COMM_DROM.proches <-
+#' transfo_om(shape_origine = COMM_DROM,
+#'            var_departement = "INSEE_DEP",
+#'            type_transfo = "v1")
+#'}
+#'
+#' @details
+#' Les millésimes du COG disponibles sont les suivants : 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019. \cr
+#'
+#' Les codes de départements renseignés dans le champ 'var_departement' doivent être compris dans la liste suivante :
+#' \itemize{
+#' \item{'971' (Guadeloupe)}
+#' \item{'972' (Martinique)}
+#' \item{'973' (Guyane)}
+#' \item{'974' (La Réunion)}
+#' \item{'975' (St-Pierre-et-Miquelon)}
+#' \item{'976' (Mayotte)}
+#' \item{'977' (Saint-Barthélémy)}
+#' \item{'978' (Saint-Martin)}}
 #'
 #' @export
 
@@ -29,7 +52,7 @@ transfo_om <- function(shape_origine, var_departement, type_transfo){
   if (nrow(shape_origine) == 0) { stop("L'objet en entrée ne doit pas être vide") }
 
   # controle de la présence du champ département
-  if ("var_departement" %in% colnames(shape_origine)) { stop("Le champ 'var_departement' de l'objet en entrée doit être renseigné") }
+  if (!!sym(var_departement) %in% colnames(shape_origine)) { stop("Le champ 'var_departement' de l'objet en entrée doit être renseigné") }
   if (!(!!sym(var_departement) %in% c("971","972","973","974","975", "976", "977", "978"))) { stop("Les codes de départements renseignés doivent être compris dans la liste suivante : {971,972,973,974,975,976,977,978} ") }
 
   # code departement converti en caractère
@@ -116,15 +139,41 @@ transfo_om <- function(shape_origine, var_departement, type_transfo){
       st_set_crs(3857)
   }
 
+  if (shape_origine %>% filter(!!sym(var_departement) %in% '977') %>% nrow() >0) {
+    shape_977 <-
+      shape_origine %>%
+      filter(!!sym(var_departement) %in% "977") %>%
+      as(., 'Spatial') %>%
+      elide(rot=param_DROM_rapp %>% filter(DEP %in% '977') %>% pull(rotation),
+            scale=param_DROM_rapp %>% filter(DEP %in% '977') %>% pull(echelle)) %>%
+      elide(shift=c(param_DROM_rapp %>% filter(DEP %in% '977') %>% pull(shift_x),
+                    param_DROM_rapp %>% filter(DEP %in% '977') %>% pull(shift_y))) %>%
+      st_as_sf(.) %>%
+      st_set_crs(3857)
+  }
+
+  if (shape_origine %>% filter(!!sym(var_departement) %in% '978') %>% nrow() >0) {
+    shape_978 <-
+      shape_origine %>%
+      filter(!!sym(var_departement) %in% "978") %>%
+      as(., 'Spatial') %>%
+      elide(rot=param_DROM_rapp %>% filter(DEP %in% '978') %>% pull(rotation),
+            scale=param_DROM_rapp %>% filter(DEP %in% '978') %>% pull(echelle)) %>%
+      elide(shift=c(param_DROM_rapp %>% filter(DEP %in% '978') %>% pull(shift_x),
+                    param_DROM_rapp %>% filter(DEP %in% '978') %>% pull(shift_y))) %>%
+      st_as_sf(.) %>%
+      st_set_crs(3857)
+  }
+
   shape_origine.rap <- rbind(if(exists("shape_971")) shape_971,
                              if(exists("shape_972")) shape_972,
                              if(exists("shape_973")) shape_973,
                              if(exists("shape_974")) shape_974,
-                             if(exists("shape_976")) shape_976) %>%
+                             if(exists("shape_976")) shape_976,
+                             if(exists("shape_977")) shape_977,
+                             if(exists("shape_978")) shape_978) %>%
     rename(!!(nom_col_geom) := geometry) %>%
     identity()
-
-
 
 }
 
